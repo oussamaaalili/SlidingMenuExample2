4447db0f4d4bf382669b8c9261d6ab84034668db
oussamaaalili <mr-aalili@live.nl>
2016-12-18 22:04:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/oussama/slidingmenuexample2/model/ItemSlideMenu.java
@@ -0,0 +1,32 @@
+package com.example.oussama.slidingmenuexample2.model;
+
+/**
+ * Created by oussama on 18-12-2016.
+ */
+
+public class ItemSlideMenu {
+
+    private int imgId;
+    private String title;
+
+    public ItemSlideMenu(int imgId, String title) {
+        this.imgId = imgId;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
